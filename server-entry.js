9743f28336daa751170cddfab877067055426d9e
ヽ◕◡◕✿ノ <stephenkamenar@gmail.com>
2026-08-31 01:29:50 -0400

    Authenticate device credentials before routed APIs

--- server-entry.js
+++ server-entry.js
@@ -1,7 +1,7 @@
+import './server-auth.js';
 import './thumbnail-server.js';
 import './collection-delete-server.js';
 import './collections-server.js';
 import './backup-policy-server.js';
 import './metadata-server.js';
-import './server-auth.js';
 await import('./server.js');
