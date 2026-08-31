e4acdcd87e01a04f5bb541e4b147e18ac0984d44
ヽ◕◡◕✿ノ <stephenkamenar@gmail.com>
2026-08-31 01:25:57 -0400

    Load device auth before server startup

--- server-entry.js
+++ server-entry.js
@@ -3,4 +3,5 @@ import './collection-delete-server.js';
 import './collections-server.js';
 import './backup-policy-server.js';
 import './metadata-server.js';
+import './server-auth.js';
 await import('./server.js');
