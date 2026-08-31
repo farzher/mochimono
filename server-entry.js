d3ee9ec28d53f68908d28aa898fa08512df8e6c6
ヽ◕◡◕✿ノ <stephenkamenar@gmail.com>
2026-08-31 00:56:32 -0400

    Load smart collection backup policies

--- server-entry.js
+++ server-entry.js
@@ -1,5 +1,6 @@
 import './thumbnail-server.js';
 import './collection-delete-server.js';
 import './collections-server.js';
+import './backup-policy-server.js';
 import './metadata-server.js';
 await import('./server.js');
