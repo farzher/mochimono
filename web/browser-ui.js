7f6582fe63eb8eb77a3704ceb3314b3ac58e6aef
ヽ◕◡◕✿ノ <stephenkamenar@gmail.com>
2026-09-10 06:55:07 -0400

    Load simplified similarity labels

--- web/browser-ui.js
+++ web/browser-ui.js
@@ -7,6 +7,7 @@ import './visual-similarity.js';
 import './context-similarity.js';
 import './similarity-sort-lock.js';
 import './similarity-sort-owned.js';
+import './similarity-sort-cleanup.js';
 import './similarity-sort-startup.js';
 import './navigation-state.js';
 import './filter-history.js';
