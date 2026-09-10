6782c78b834a9fb6ecd3b425aec92ae454c06a87
ヽ◕◡◕✿ノ <stephenkamenar@gmail.com>
2026-09-10 05:08:56 -0400

    Load visual similarity search

--- web/browser-ui.js
+++ web/browser-ui.js
@@ -3,6 +3,7 @@ import './random-sort.js';
 import './viewer-grid-return.js';
 import './browser-folder-client-loader.js';
 import './browser-provenance.js';
+import './visual-similarity.js';
 import './navigation-state.js';
 import './filter-history.js';
 import './collection-history.js';
