dd89a3e7ca34b322e202be37aa92862833d05ab8
ヽ◕◡◕✿ノ <stephenkamenar@gmail.com>
2026-09-10 06:46:06 -0400

    Load similarity sort lock

--- web/browser-ui.js
+++ web/browser-ui.js
@@ -5,6 +5,7 @@ import './browser-folder-client-loader.js';
 import './browser-provenance.js';
 import './visual-similarity.js';
 import './context-similarity.js';
+import './similarity-sort-lock.js';
 import './similarity-sort-owned.js';
 import './similarity-sort-startup.js';
 import './navigation-state.js';
