d6e2e0a8287d48559473cfb8c0af346fc943ccf4
ヽ◕◡◕✿ノ <stephenkamenar@gmail.com>
2026-09-10 03:15:01 -0400

    Load browser catalog reconciliation before sync

--- web/browser-folder-client-loader.js
+++ web/browser-folder-client-loader.js
@@ -1,3 +1,6 @@
 if (document.documentElement.classList.contains('client-library')) {
-  import('./browser-folder-drop.js').catch(error => console.warn('Browser folder sync unavailable', error));
+  import('./browser-folder-catalog-reconcile.js')
+    .catch(error => console.warn('Browser folder catalog reconciliation unavailable', error))
+    .then(() => import('./browser-folder-drop.js'))
+    .catch(error => console.warn('Browser folder sync unavailable', error));
 }
