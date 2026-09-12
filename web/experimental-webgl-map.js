02f98cc3a9d92d75399b583c3142a54495e915b3
ヽ◕◡◕✿ノ <stephenkamenar@gmail.com>
2026-09-12 18:41:06 -0400

    Enable continuous original quality while zooming

--- web/experimental-webgl-map.js
+++ web/experimental-webgl-map.js
@@ -1,3 +1,3 @@
 import './experimental-fullscreen-view.js';
 import './experimental-performance-guard.js';
-export { ExperimentalWebGLMapRenderer } from './experimental-webgl-map-v9.js';
+export { ExperimentalWebGLMapRenderer } from './experimental-webgl-map-v10.js';
