ed0aea3bc0bb271c7f2b421a1f8c3b47d782ebb4
ヽ◕◡◕✿ノ <stephenkamenar@gmail.com>
2026-09-16 20:51:11 -0400

    Fix browser folder escaping

--- agent-web/browser-folders.js
+++ agent-web/browser-folders.js
@@ -21,7 +21,7 @@ if (folders) {
   style.textContent = `.browser-folder-item .storage-meta:empty{display:none!important}`;
   document.head.append(style);
 
-  const esc = value => String(value ?? '').replace(/[&<>"']/g, char => ({ '&':'&amp;','<':'&lt;','>':'&gt;','"':'&quot',"'":'&#39;' }[char]));
+  const esc = value => String(value ?? '').replace(/[&<>"']/g, char => ({ '&':'&amp;','<':'&lt;','>':'&gt;','"':'&quot;',"'":'&#39;' }[char]));
   const normalize = source => source ? { ...source, scope:source.scope === 'all' ? 'all' : 'media', cloud:source.cloud === true } : source;
 
   function bytes(number) {
